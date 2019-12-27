4bcce3dc5d954f19ab62c4ab00e3f982705e8ee4
github username <gavinswhitlock@gmail.com>
2019-12-27 10:54:47 -0600

    added the base code for querying API using HTTParty gem

---
+++ app/models/flight.rb
@@ -0,0 +1,16 @@
+require 'httparty'
+require 'awesome_print'
+
+class Flight
+    include HTTParty
+    base_uri "http://api.travelpayouts.com/v1/prices/cheap?"
+    
+    def flight_search
+        #self.class.get("origin=#{origin}&destination=#{destination}&depart_date=#{departure_date}&return_date=#{return_date}&token=#{API_TOKEN}")
+        self.class.get("origin=HOU&destination=LAX&depart_date=2020-01&return_date=2020-02&token=cc1f7b57d0a00136c286c8fe4ae4fc06")
+    end
+end
+
+test_flight = Flight.new
+
+puts test_flight.flight_search
