123bf53202b1a16659f75e2d6c5c496cb9c0ef04
Chance Mims <chance@Chances-MacBook-Air.local>
2019-12-31 08:27:24 -0600

    added basic structure for app menu. These function calls should help guide other model creation

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,7 @@
 require 'bundler'
 Bundler.require
 
+require_relative '../app/flightiron_app.rb'
+
 ActiveRecord::Base.establish_connection(adapter: 'sqlite3', database: 'db/development.db')
 require_all 'lib'
